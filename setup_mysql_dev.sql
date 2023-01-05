9b46aa8c6e58a38b3b7b18e0b393030c306fceb4
Daniel-timmy <ajayitimmy45@gmail.com>
2023-01-05 13:01:31 +0000

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,7 @@
---Prepares a MySQL server
+-- prepares a MySQL server
 -- create project developement database with the name : hbnb_dev_db
-
-CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,8 @@
---Prepares a MySQL server
+-- prepares a MySQL server
 -- create project developement database with the name : hbnb_test_db
 
-CREATE DATABASE IF NOT EXIST hbnb_test_db;
-CREATE USER IF NOT EXIST 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
