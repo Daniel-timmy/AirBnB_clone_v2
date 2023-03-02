72c00b1625094d757638aa6202f1e2888e7f125e
Daniel-timmy <ajayitimmy45@gmail.com>
2023-03-02 12:29:24 +0100

    web flask899

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Starts a flask web application"""
 
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Return hello HBNB"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
